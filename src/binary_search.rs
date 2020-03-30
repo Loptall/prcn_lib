77d71169dfd7d5b66c91c3295f5bbee1884b498f
Loptall <yutapypy0930@gmail.com>
2020-03-30 11:10:38 +0900

    upper_bound and lower_bound

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -28,5 +28,39 @@ fn binary_search_test() {
     let v = vec![1, 3, 3, 3, 4, 6, 7, 7, 8, 10];
     // println!("{}", -1 / 2);
     assert_eq!(4, binary_search(v.len() as _, -1, |i| v[i as usize] > 3));
-    assert_eq!(3, binary_search(v.len() as _, -1, |i| v[i as usize] >= 3));
+    assert_eq!(1, binary_search(v.len() as _, -1, |i| v[i as usize] >= 3));
+}
+
+/// vの要素について最初に現れたval以上の要素のインデックスを返す
+pub fn lower_bound<T: PartialOrd>(v: &[T], val: &T) -> usize {
+    (binary_search(-1i64, v.len() as i64, |i| v[i as usize] < *val) + 1) as usize
+}
+
+#[test]
+fn lower_bound_test() {
+    let v: &[i32] = &[1, 3, 3, 4, 5];
+    assert_eq!(lower_bound(v, &0), 0);
+    assert_eq!(lower_bound(v, &1), 0);
+    assert_eq!(lower_bound(v, &2), 1);
+    assert_eq!(lower_bound(v, &3), 1);
+    assert_eq!(lower_bound(v, &4), 3);
+    assert_eq!(lower_bound(v, &5), 4);
+    assert_eq!(lower_bound(v, &999), 5);
+}
+
+/// vの要素について最初に現れたvalより大きい要素のインデックスを返す
+pub fn upper_bound<T: PartialOrd>(v: &[T], val: &T) -> usize {
+    (binary_search(-1i64, v.len() as i64, |i| v[i as usize] <= *val) + 1) as usize
+}
+
+#[test]
+fn upper_bound_test() {
+    let v: &[i32] = &[1, 3, 3, 4, 5];
+    assert_eq!(upper_bound(v, &0), 0);
+    assert_eq!(upper_bound(v, &1), 1);
+    assert_eq!(upper_bound(v, &2), 1);
+    assert_eq!(upper_bound(v, &3), 3);
+    assert_eq!(upper_bound(v, &4), 4);
+    assert_eq!(upper_bound(v, &5), 5);
+    assert_eq!(upper_bound(v, &999), 5);
 }
